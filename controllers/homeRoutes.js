7fe87218f51395ead003534fb55af972bf42cc50
Duane Cantera <duane.cantera@comcast.net>
2021-12-27 17:02:04 -0500

    I built the main page and got it to display.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+/**************************************************************
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -19,6 +20,25 @@ router.get('/', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+******************************************************************************/
+
+router.get('/', async (req, res) => {
+  try {
+    /*****************************************************
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+    *************************************************************************/
+
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
---
+++ server.js
@@ -0,0 +1,79 @@
+// Import the path Module:
+const path = require('path');
+
+// Import the express Module:
+const express = require('express');
+
+// Import the express-session Module:
+const session = require('express-session');
+
+// Import the express-handlebars Module:
+const exphbs = require('express-handlebars');
+
+// Import all of the Controller Routes.
+const routes = require('./controllers');
+
+// Import all of the Sequelize Configuration Connection Module.
+const sequelize = require('./config/connection');
+
+// Import the Sequelize Session Store Module:
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Create the express application variable.
+const app = express();
+
+// Create the constant variable for the port number
+const PORT = process.env.PORT || 3001;
+
+// Create the express handlebar.
+const hbs = exphbs.create({});
+
+// Sets up session and connect to our Sequelize db
+const sess = {
+  secret: 'Super secret secret',
+  // Express session will use cookies by default, but we can specify options for those cookies by adding a cookies property 
+  // to our session options.
+  cookie: {
+    // maxAge sets the maximum age for the session to be active. Listed in milliseconds.
+    maxAge: 3600,
+    // httpOnly tells express-session to only store session cookies when the protocol being used to connect to the server is HTTP.
+    httpOnly: true,
+    // secure tells express-session to only initialize session cookies when the protocol being used is HTTPS. Having this set to true, 
+    // and running a server without encryption will result in the cookies not showing up in your developer console.    
+    secure: false,
+    // sameSite tells express-session to only initialize session cookies when the referrer provided by the client matches the domain
+    // out server is hosted from.
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+// Add the session to the express application server.
+app.use(session(sess));
+
+// Adding the handlebar engine to the application engine.
+// Telling the application that the filename extension is handlebars.
+app.engine('handlebars', hbs.engine);
+
+// Setting the view engine to handlebars.
+app.set('view engine', 'handlebars');
+
+// Setting up the middleware.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Setting up the static path to be the public directory.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Setting up the routes.
+app.use(routes);
+
+// sync up sequelize - force equals false so data doesn't get dropped on every sync.
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
