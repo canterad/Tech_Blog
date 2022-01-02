3f7b00b609e94386635d6b386d7268799536ae7b
Duane Cantera <duane.cantera@comcast.net>
2022-01-02 16:41:45 -0500

    Made change to code because not working on Heroku.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -179,6 +179,7 @@ router.get('/blog/:id', async (req, res) => {
     let userId = req.session.user_id;
     let blogId = req.params.id;
     let szResult = "";
+    let szTemp = "";
 
     if (req.params.id != "0")
     {
@@ -191,7 +192,8 @@ router.get('/blog/:id', async (req, res) => {
 
        // Need to replace the '<br>' characters with '\n' characters.
        // Going from paragraph element to text area element. Added to change file.
-       szResult = blogItem.content.replaceAll('<br>', '\n');
+       szTemp = blogItem.content;
+       szResult = szTemp.replaceAll('<br>', '\n');
        blogItem.content = szResult;
 
         res.render('blog', {blogItem, loggedIn: req.session.logged_in,
